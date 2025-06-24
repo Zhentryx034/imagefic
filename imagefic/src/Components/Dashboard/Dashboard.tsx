bd038dfd5af304f46f64a45334ad4aca893a3ad2
mrauthentik <umokeuchenna2020@gmail.com>
2025-06-24 12:24:57 +0100

    Refactor Dashboard component to wrap logo in a Link for navigation

--- imagefic/src/Components/Dashboard/Dashboard.tsx
+++ imagefic/src/Components/Dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/Zhentryx.png';
 import userImg from '../../assets/Pictures/dashboard images/user.png'
 import img1 from '../../assets/Pictures/dashboard images/dashboard-img (1).png'
@@ -21,7 +22,7 @@ const Dashboard: React.FC = () => {
      <div className='font-["Poppins"] bg-white h-screen'>
         <nav>
             <div className='flex justify-between items-center bg-white p-4 shadow-md fixed top-0 left-0 right-0 z-50 h-24'>
-                <img src={logo} alt="Logo" className='w-[10%] ml-20' />
+              <Link to='/' className='w-full'><img src={logo} alt="Logo" className='w-[10%] ml-20' /></Link>
                 <div className='flex items-center space-x-4 font-semibold cursor-pointer text-[#333] mr-4'>
                     <p className='hover:text-purple-600 hover:scale-105'>Home</p>
                     <p className='hover:text-purple-600 hover:scale-105'>Explore</p>

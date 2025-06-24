193a0241536ece1af6c5c1e07faa4186432d00ab
mrauthentik <umokeuchenna2020@gmail.com>
2025-06-24 12:26:29 +0100

    Adjust margin for navigation items in Dashboard component for improved layout

--- imagefic/src/Components/Dashboard/Dashboard.tsx
+++ imagefic/src/Components/Dashboard/Dashboard.tsx
@@ -23,7 +23,7 @@ const Dashboard: React.FC = () => {
         <nav>
             <div className='flex justify-between items-center bg-white p-4 shadow-md fixed top-0 left-0 right-0 z-50 h-24'>
               <Link to='/' className='w-full'><img src={logo} alt="Logo" className='w-[10%] ml-20' /></Link>
-                <div className='flex items-center space-x-4 font-semibold cursor-pointer text-[#333] mr-4'>
+                <div className='flex items-center space-x-4 font-semibold cursor-pointer text-[#333] mr-12'>
                     <p className='hover:text-purple-600 hover:scale-105'>Home</p>
                     <p className='hover:text-purple-600 hover:scale-105'>Explore</p>
                     <p className='hover:text-purple-600 hover:scale-105'>Create</p>

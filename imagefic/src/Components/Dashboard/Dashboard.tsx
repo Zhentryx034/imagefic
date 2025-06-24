f762a1e7b46d6af01faac1a40ffd42ca5c2eb81b
mrauthentik <umokeuchenna2020@gmail.com>
2025-06-24 12:00:16 +0100

    Add height to navigation bar in Dashboard component

--- imagefic/src/Components/Dashboard/Dashboard.tsx
+++ imagefic/src/Components/Dashboard/Dashboard.tsx
@@ -6,7 +6,7 @@ const Dashboard: React.FC = () => {
   return (
      <div className='font-["Poppins"] bg-[#F5F5F5] h-screen'>
         <nav>
-            <div className='flex justify-between items-center bg-white p-4 shadow-md fixed top-0 left-0 right-0 z-50'>
+            <div className='flex justify-between items-center bg-white p-4 shadow-md fixed top-0 left-0 right-0 z-50 h-24'>
                 <img src={logo} alt="Logo" className='w-[15%] ml-20' />
                 <div className='flex items-center space-x-4 font-semibold cursor-pointer text-[#333] mr-4'>
                     <p>Home</p>

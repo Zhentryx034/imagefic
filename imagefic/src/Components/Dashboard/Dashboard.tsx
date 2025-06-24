d26f7fcbba0835663cf36eac0eb092bf23e545d7
mrauthentik <umokeuchenna2020@gmail.com>
2025-06-24 12:11:07 +0100

    Refactor Dashboard component to include image items and update layout

--- imagefic/src/Components/Dashboard/Dashboard.tsx
+++ imagefic/src/Components/Dashboard/Dashboard.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import logo from '../../assets/Zhentryx.png';
 import userImg from '../../assets/Pictures/dashboard images/user.png'
+import img1 from '../../assets/Pictures/dashboard images/dashboard-img (1).png'
+import img2 from '../../assets/Pictures/dashboard images/dashboard-img (2).png'
+import img3 from '../../assets/Pictures/dashboard images/dashboard-img (3).png' 
+import img4 from '../../assets/Pictures/dashboard images/dashboard-img (4).png'
+import img5 from '../../assets/Pictures/dashboard images/dashboard-img (5).png'
+import img6 from '../../assets/Pictures/dashboard images/dashboard-img (6).png'
 
 const Dashboard: React.FC = () => {
+    const items = [
+        {img: img1, title: "Presentation" },
+        {img: img2, title: "Cars" },
+        {img: img3, title: "Illustration" },
+        {img: img4, title: "Art" },
+        {img: img5, title: "Sport" },
+        {img: img6, title: "Nature" }
+    ]
   return (
-     <div className='font-["Poppins"] bg-[#F5F5F5] h-screen'>
+     <div className='font-["Poppins"] bg-white h-screen'>
         <nav>
             <div className='flex justify-between items-center bg-white p-4 shadow-md fixed top-0 left-0 right-0 z-50 h-24'>
-                <img src={logo} alt="Logo" className='w-[15%] ml-20' />
+                <img src={logo} alt="Logo" className='w-[10%] ml-20' />
                 <div className='flex items-center space-x-4 font-semibold cursor-pointer text-[#333] mr-4'>
                     <p>Home</p>
                     <p>Explore</p>
@@ -17,6 +31,19 @@ const Dashboard: React.FC = () => {
                 </div>
             </div>
         </nav>
+        <div className='mt-36 pl-6 pr-6 '>
+            <h1 className='text-[36px] font-bold mt-8 text-left pl-8 '>Dashboard</h1>
+            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6 bg-white'>
+                {items.map((item, index) => (
+                    <div key={index} className='bg-white rounded-lg shadow-md overflow-hidden'>
+                        <img src={item.img} alt={item.title} className='w-full h-48 object-cover' />
+                        <div className='p-4'>
+                            <h2 className='text-lg font-semibold'>{item.title}</h2>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
      </div>
   );
 }

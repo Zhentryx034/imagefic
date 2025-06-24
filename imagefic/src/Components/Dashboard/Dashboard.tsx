f73f57eda6c54badf01813b4cf23208dda0d2776
mrauthentik <umokeuchenna2020@gmail.com>
2025-06-24 12:22:54 +0100

    Enhance Dashboard component styling with hover effects on navigation items and item images

--- imagefic/src/Components/Dashboard/Dashboard.tsx
+++ imagefic/src/Components/Dashboard/Dashboard.tsx
@@ -23,9 +23,9 @@ const Dashboard: React.FC = () => {
             <div className='flex justify-between items-center bg-white p-4 shadow-md fixed top-0 left-0 right-0 z-50 h-24'>
                 <img src={logo} alt="Logo" className='w-[10%] ml-20' />
                 <div className='flex items-center space-x-4 font-semibold cursor-pointer text-[#333] mr-4'>
-                    <p>Home</p>
-                    <p>Explore</p>
-                    <p>Create</p>
+                    <p className='hover:text-purple-600 hover:scale-105'>Home</p>
+                    <p className='hover:text-purple-600 hover:scale-105'>Explore</p>
+                    <p className='hover:text-purple-600 hover:scale-105'>Create</p>
                 <img src={userImg} alt="User" className='w-10 h-10 rounded-full' />
                
                 </div>
@@ -36,7 +36,7 @@ const Dashboard: React.FC = () => {
             <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 p-6 bg-white'>
                 {items.map((item, index) => (
                     <div key={index} className=' overflow-hidden'>
-                        <img src={item.img} alt={item.title} className='w-[100%] h-68 object-cover rounded-lg shadow-md ' />
+                        <img src={item.img} alt={item.title} className='w-[100%] h-68 object-cover rounded-lg shadow-md hover:scale-105' />
                         <div className='p-4'>
                             <h2 className='text-[26px] font-semibold text-[#333]'>{item.title}</h2>
                         </div>
